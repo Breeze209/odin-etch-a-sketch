4354eb8f272ac37cfaf7031f039c0e52b1ec5eb0
Breeze209 <236219025+Breeze209@users.noreply.github.com>
2025-10-22 08:51:24 +0200

    Solve the a bug
    The divs wont render when the pixelRow was bigger then 10.
    The bug was fixed by changing the max pixels to max pixelRow.

---
+++ etch-a-sketch.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.divs');
+const changeBtn = document.querySelector('.change');
+let divs;
+
+function getPixelAmount() {
+    let pixelsRow = prompt('How many pixels do you want to have? (Max: 100)');
+    pixelsRow = Number(pixelsRow);
+    let pixels = pixelsRow * pixelsRow;
+    let newDiv;
+
+    if (!Number.isInteger(pixels) || pixels <= 0) {
+        alert('ERROR! You need to input a number');
+    } else if (pixelsRow > 100) {
+        pixels = 100;
+    } else {
+        for (let i = 1; i <= pixels; i++) {
+            newDiv = document.createElement('div');
+            newDiv.classList.add('newDiv');
+            newDiv.style.flexBasis = `${100 / pixelsRow}%`;
+            newDiv.style.backgroundColor  ='pink';
+
+            container.appendChild(newDiv);
+        }
+    }
+    divs = document.querySelectorAll('.newDiv');
+}
+
+changeBtn.addEventListener('click', () => {
+    if (divs) {
+        divs.forEach(element => {
+            element.remove();
+        });
+        getPixelAmount();
+    } else {
+        getPixelAmount();
+    }
+});
